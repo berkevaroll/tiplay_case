d2d010756a6c4bff981f335b7fef913f281cd7aa
berkevaroll <berkevaroll@gmail.com>
2020-10-30 12:32:34 +0300

    New Parabola Lib was used. Ball movement is done

--- Assets/TrajectoryController.cs
+++ Assets/TrajectoryController.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class TrajectoryController : MonoBehaviour
 {
-    [Header("Line renderer variables")]
-    public LineRenderer line;
-    public int resolution;
 
-    [Header("Physics variables")]
-    public Vector3 velocity;
-    public float yLimit;
-    public float g;
+
+    [Header("Objects")]
+    public GameObject startPoint;
+    public GameObject midPoint;
+    public GameObject endPoint;
+    public LineRenderer line;
 
     private float mouseX;
     private float mouseY;
@@ -20,51 +19,55 @@ public class TrajectoryController : MonoBehaviour
     private float lastY;
     private void Start()
     {
-        g = Mathf.Abs(Physics.gravity.y);
 
     }
     private void Update()
     {
-        StartCoroutine(RenderArc());
-        
+
+
 
         if (Input.GetMouseButton(0))
         {
+
             lastX = Input.GetAxis("Mouse X");
             lastY = Input.GetAxis("Mouse Y");
-            velocity.x += -lastX  * .8f;
-            velocity.z += -lastY * 2f;
+            Vector3 myMid = midPoint.transform.position;
+            Vector3 myEnd = endPoint.transform.position;
+            midPoint.transform.position = new Vector3(myMid.x - lastY, myMid.y, myMid.z + lastX * 1.2f);
+            endPoint.transform.position = new Vector3(myEnd.x - lastY, myEnd.y, myEnd.z + lastX * .6f);
+
         }
+        //StartCoroutine(RenderArc());
         if (Input.GetMouseButtonUp(0))
         {
-            //ball movement
+            GetComponent<ParabolaController>().FollowParabola();
         }
 
     }
-    private IEnumerator RenderArc()
-    {
-        line.positionCount = resolution + 1;
-        line.SetPositions(CalculateLineArray());
-        yield return null;
-    }
+    //private IEnumerator RenderArc()
+    //{
+    //    line.positionCount = resolution + 1;
+    //    line.SetPositions(CalculateLineArray());
+    //    yield return null;
+    //}
 
-    private Vector3[] CalculateLineArray()
-    {
+    //private Vector3[] CalculateLineArray()
+    //{
         
         
-        Vector3[] lineArray = new Vector3[resolution + 1];
-        for (int i = 0; i < lineArray.Length; i++)
-        {
-            var t = i / (float)lineArray.Length;
-            lineArray[i] = CalculateLinePoint(t);
-        }
-        return lineArray;
-    }
-    private Vector3 CalculateLinePoint(float t)
-    {
-        float z = velocity.z * t;
-        float x = (velocity.x * t) - (g * Mathf.Pow(t, 2) / 2);
-        return new Vector3((x + transform.position.x),transform.position.y ,(z +transform.position.z));
+    //    Vector3[] lineArray = new Vector3[resolution + 1];
+    //    for (int i = 0; i < lineArray.Length; i++)
+    //    {
+    //        var t = i / (float)lineArray.Length;
+    //        lineArray[i] = CalculateLinePoint(t);
+    //    }
+    //    return lineArray;
+    //}
+    //private Vector3 CalculateLinePoint(float t)
+    //{
+    //    float z = velocity.z * t;
+    //    float x = (velocity.x * t) - (g * Mathf.Pow(t, 2) / 2);
+    //    return new Vector3((x + transform.position.x),transform.position.y ,(z +transform.position.z));
 
-    }
+    //}
 }
